c5a401663f85b4a3127844de7710075e125b56c1
AhnafK <akazi1@stuy.edu>
2019-01-15 11:10:15 -0500

    colors

--- testboard.c
+++ testboard.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int build(char board[8][8]){
   for(int y = 0; y < 8; y++){
@@ -43,40 +44,41 @@ int place(char board[8][8], char x, char y, char length, int rot){
 
 int hit(char board[8][8], int x, int y){
   if(board[x][y] == 1){
-    printf("%d HIT!\n", board[x][y]);
+    printf("HIT!\n");
     board[x][y] = 2;
-    if(board[x-1][y] != 1 && board[x+1][y] != 1 && board[x][y-1] != 1 && board[x][y+1] != 1){
-      printf("Ship sunk!");
-    }
+    //if(board[x-1][y] != 1 && board[x+1][y] != 1 && board[x][y-1] != 1 && board[x][y+1] != 1){
+    //  printf("Ship sunk!");
+    //}
     return 1;
   }
   else{
-    printf("%d MISS!\n", board[x][y]);
+    printf("MISS!\n");
     board[x][y] = 3;
     return 0;
   }
 }
 
 int display(char board[8][8], char op){
-  printf("\n  0 1 2 3 4 5 6 7\n");
+  printf("\n\e[0m  0 1 2 3 4 5 6 7\n");
+  char * standards[] = {"\e[106m", "\e[100m", "\e[101m", "\e[47m"};
   for(int y = 0; y < 8; y++){
-    printf("%d ", y);
+    printf("\e[0m%d ", y);
     for(int x = 0; x < 8; x++){
       if(op){
 	printf("%d ", board[x][y]);
       }
       else{
 	if(board[x][y] == 1){
-	  printf("0 ");
+	  printf("\e[106m0 ");
 	}
 	else{
-	  printf("%d ", board[x][y]);
+	  printf("%s%d ", standards[board[x][y]], board[x][y]);
 	}
       }
     }
     printf("\n");
   }
-  printf("\n");
+  printf("\n\e[0m");
 }
 
 int initialize(char board[8][8]){
@@ -102,6 +104,20 @@ int initialize(char board[8][8]){
   return 0;
 }
 
+int rinit(char board[8][8]){
+  srandom(time(NULL));
+  char* boats[5] = {"Battleship", "Destroyer", "Submarine", "Patrol Boat", "Carrier"};
+  int sizes[5] = {4,3,3,2,5};
+  for(int s = 0; s < 5; s++){
+    int good = place(board, random()%8,random()%8,sizes[s],random()%2);
+    while(good == -1){
+      good = place(board, random()%8,random()%8,sizes[s],random()%2);
+    }
+    good = 0;
+  }
+  return 0;
+}
+
 int main(){
   int redscore = 17;
   int bluescore = 17;
@@ -109,14 +125,21 @@ int main(){
   char blue[8][8];
   build(blue);
   build(red);
-  initialize(red);
-  display(red, 1);
+  //initialize(red);
+  //display(red, 1);
   int tarx;
   int tary;
-  while(!(redscore && bluescore)){
-    display(red, 0);
+  rinit(blue);
+  display(blue, 1);
+  system("clear");
+  display(blue, 0);
+  printf("Select your target: ");
+  scanf("%d %d", &tarx, &tary);
+  while(redscore && bluescore){
+    system("clear");
+    hit(blue, tarx, tary);
+    display(blue, 0);
     printf("Select your target: ");
     scanf("%d %d", &tarx, &tary);
-    hit(red, tarx, tary);
   }
 }
